4698cf0e4f8daa240303fba25d0011282b6c48bb
howellsy <jamie.howells@gmail.com>
2023-11-14 12:12:57 +0000

    Initial unit tests for depositCollateral()

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralisedStablecoin} from "../../src/DecentralisedStablecoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,12 +16,15 @@ contract DSCEngineTest is Test {
     address ethUsdPriceFeed;
     address weth;
 
-    address public user = address(1);
+    address public constant USER = address(1);
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
         (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     /**
@@ -33,5 +37,28 @@ contract DSCEngineTest is Test {
         uint256 expectedUsdValue = 30000e18;
         uint256 actualUsdValue = dsce.getUsdValue(weth, ethAmount);
         assertEq(actualUsdValue, expectedUsdValue);
+    }
+
+    /**
+     * ========================
+     * Deposit Collateral tests
+     * ========================
+     */
+    function testDepositRevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__MustBeMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
+    function testDepositRevertsIfTokenNotAllowed() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__TokenNotAllowed.selector);
+        dsce.depositCollateral(address(0), AMOUNT_COLLATERAL);
+        vm.stopPrank();
     }
 }
