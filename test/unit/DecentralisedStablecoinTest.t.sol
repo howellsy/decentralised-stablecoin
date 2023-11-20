9773be86f5b446ad29a8044202cadd1debcc5fc0
howellsy <jamie.howells@gmail.com>
2023-11-20 16:24:15 +0000

    Unit tests for DecentralisedStablecoin

---
+++ test/unit/DecentralisedStablecoinTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DecentralisedStablecoin} from "../../src/DecentralisedStablecoin.sol";
+
+contract DecentralisedStablecoinTest is Test {
+    DecentralisedStablecoin dsc;
+
+    function setUp() public {
+        dsc = new DecentralisedStablecoin();
+    }
+
+    function testBurnAmountMustExceedBalance() public {
+        vm.expectRevert(DecentralisedStablecoin.DecentralisedStablecoin__BurnAmountExceedsBalance.selector);
+        dsc.burn(1);
+    }
+
+    function testMustMintMoreThanZero() public {
+        vm.expectRevert(DecentralisedStablecoin.DecentralisedStablecoin__MustBeMoreThanZero.selector);
+        dsc.mint(address(this), 0);
+    }
+
+    function testMustBurnMoreThanZero() public {
+        vm.expectRevert(DecentralisedStablecoin.DecentralisedStablecoin__MustBeMoreThanZero.selector);
+        dsc.burn(0);
+    }
+
+    function testCannotMintToZeroAddress() public {
+        vm.expectRevert(DecentralisedStablecoin.DecentralisedStablecoin__NotZeroAddress.selector);
+        dsc.mint(address(0), 1);
+    }
+}
